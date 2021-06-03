bb27c0a7b395ce79b1d8cac5c337f1c83e16703f
julieta <julietaandrebogado@gmail.com>
2021-06-03 01:06:34 -0300

    modigique el css del body del index.php

--- index.php
+++ index.php
@@ -92,19 +92,19 @@
     <!-- Example row of columns -->
     <div class="row">
       <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Will you do the same for me? It's time to face the music I'm no longer your muse. Heard it's beautiful, be the judge and my girls gonna take a vote. I can feel a phoenix inside of me. Heaven is jealous of our love, angels are crying from up above. Yeah, you take me to utopia.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        
+        <p>Guardá tu vehículo con seguridad y tranquilidad.</p>
+        <p><a class="btn btn-secondary" href="#" role="button">Ver más &raquo;</a></p>
       </div>
       <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Standing on the frontline when the bombs start to fall. Heaven is jealous of our love, angels are crying from up above. Can't replace you with a million rings. Boy, when you're with me I'll give you a taste. There’s no going back. Before you met me I was alright but things were kinda heavy. Heavy is the head that wears the crown.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        
+        <p>Conocé los estacionamientos disponibles en Ciudad Autónoma de Buenos Aires, Gran Buenos Aires e interior del país, y guardá tu auto con tranquilidad y seguridad.</p>
+        <p><a class="btn btn-secondary" href="#" role="button">Más detalles &raquo;</a></p>
       </div>
       <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Playing ping pong all night long, everything's all neon and hazy. Yeah, she's so in demand. She's sweet as pie but if you break her heart. But down to earth. It's time to face the music I'm no longer your muse. I guess that I forgot I had a choice.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        
+        <p>Más de 100 años lideres en la empresa.</p>
+        <p><a class="btn btn-secondary" href="#" role="button">Conocenos &raquo;</a></p>
       </div>
     </div>
 
